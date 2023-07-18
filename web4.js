36c1c77b3e70faa473319c8ac46228d0b9eaa2e8
“koding88” <duongngocanh2k03@gmail.com>
2023-07-18 11:27:41 +0700

    Fix port web4.js

--- web4.js
+++ web4.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const fs = require('fs');
-const port = 3000;
+const port = process.env.port || 3001;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
